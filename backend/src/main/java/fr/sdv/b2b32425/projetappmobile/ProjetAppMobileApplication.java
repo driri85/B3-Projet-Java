853a47b12d3691d34b9a46e95c3150a32094848f
dridri85 <adrirando@gmail.com>
2025-07-27 15:36:55 +0200

    Update ProjetAppMobileApplication.java

--- backend/src/main/java/fr/sdv/b2b32425/projetappmobile/ProjetAppMobileApplication.java
+++ backend/src/main/java/fr/sdv/b2b32425/projetappmobile/ProjetAppMobileApplication.java
@@ -7,8 +7,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 @SpringBootApplication
 public class ProjetAppMobileApplication implements CommandLineRunner {
+
+    @Configuration
+    public class WebConfig {
+        
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedOrigins("https://java-frontend.arsdv.site")
+                            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                            .allowedHeaders("*");
+                }
+            };
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetAppMobileApplication.class, args);
