22eaf0935c7a67efb06221d9de5fd1aad0b12f24
dridri85 <adrirando@gmail.com>
2025-07-30 16:29:28 +0200

    CORS Update

---
+++ backend/src/main/java/fr/sdv/b2b32425/projetappmobile/config/WebConfig.java
@@ -0,0 +1,23 @@
+package fr.sdv.b2b32425.projetappmobile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://java-frontend.arsdv.site")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
